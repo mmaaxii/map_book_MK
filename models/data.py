7638e541b2eb0a174819a69a8eab028314936069
mmaaxii <Maksymilian.kp@op.pl>
2024-04-18 13:10:07 +0200

    feat added functionality

---
+++ models/data.py
@@ -0,0 +1,6 @@
+users: list[dict] = [
+    {"name": "Janek", "surname": "Mielec", "posts": 20},
+    {"name": "Maciej", "surname": "Przybytek", "posts": 45},
+    {"name": "Bartosz", "surname": "Pietrasik", "posts": 60},
+    {"name": "Tymoteusz", "surname": "Miszczak", "posts": 21},
+    {"name": "Mateusz", "surname": "Matysiak", "posts": 33},
